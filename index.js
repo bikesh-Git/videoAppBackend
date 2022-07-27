15504ab6ccd92670b35ecfb755eddac51fbb323d
bikesh verma <bikesh@touchpoinystechnology.com>
2022-07-27 23:44:21 +0530

    dfdfdafasdsd

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use((err,req,res,next)=>{
     })
 })
 
-app.listen(process.env.NODE_ENV || 3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     connect()
     console.log("connected to server")
 })
