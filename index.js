ae8fe7227e6838a293b5591611dda90a38e308c0
bikesh verma <bikesh@touchpoinystechnology.com>
2022-08-03 18:20:14 +0530

    make it better

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 
 // app.set("trust proxy",1)
 
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
+app.use(cors({credentials: true, origin: '*'}))
 
 const connect=() =>{
     mongoose.connect(process.env.MONGO).then(()=>{
